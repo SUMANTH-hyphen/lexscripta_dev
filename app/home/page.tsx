101c645573641419f89760e8cb262adafe382abd
sumanth.k2019@gmail.com <sumanth.k2019@gmail.com>
2025-03-09 03:22:42 +0530

    responsive home page done

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,27 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 const PageHome = () => {
+
   return (
-    <div>page</div>
+    <div className=' relative max-h-screen max-w-screen '>
+      <Image
+        src="/home_bg.jpg"
+        width={2600}
+        height={1600}
+        alt="home screen bg"
+        className=' h-screen w-screen object-none md:object-cover'
+      />
+      <div className=' bg-black bg-opacity-45 h-full w-full absolute top-0 left-0 ' />
+      <div className=' absolute top-1/3 flex flex-col gap-5 items-center justify-center text-center left-1/2 transform -translate-x-1/2 whitespace-nowrap lg:gap-10 lg:items-start lg:top-1/3 lg:left-[60%] lg:translate-x-0  '>
+        <div className=' text-white flex flex-col gap-5 items-center justify-center lg:items-start lg:gap-10 '>
+          <div className=' text-5xl font-extralight lg:text-6xl lg:font-light'>The Law Firm</div>
+          <div className=' text-lg font-extralight '>A free template solely for your law firm</div>
+        </div>
+        <Button variant="ghost" className=' text-white hover:text-black border-white border-1 px-4 py-7 tracking-[0.2em] font-thin '>FREE CONSULTATION</Button>
+      </div>
+    </div>
   )
 }
 
